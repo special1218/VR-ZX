2ab7c16a39945dc8c0eec63ced527e53709d096f
wuqing <2634150418@qq.com>
2017-12-26 19:35:51 +0800

    view+vue组件

---
+++ src/view/VR-zx/HelloWorld.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+    <top1></top1>
+    <nav1></nav1>
+    <main1></main1>
+    <footer1></footer1>
+  </div>
+</template>
+
+<script>
+	
+	import top1 from './top.vue'
+	import nav1 from './nav.vue'
+	import main1 from './main.vue'
+	import footer1 from './footer.vue'
+	
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your sunjiabnao'
+    }
+  },
+  components:{
+  	top1,
+  	nav1,
+  	main1,
+  	footer1
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
