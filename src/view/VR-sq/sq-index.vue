465fdd5c011ba0f5f071ff5006ad7a3c3357d13e
sunjiabao <202177811@qq.com>
2017-12-28 20:17:07 +0800

    sd

---
+++ src/view/VR-sq/sq-index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    VR社区
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style scoped="scoped">
+*{
+	margin: 0;
+	padding: 0;
+}
+</style>
